908c97880514c7410453e389bbdbcc7a8a1ae13d
Thomas Eses <thomas.eses@lead-dealer.com>
2026-07-15 11:12:53 +0200

    Änderungen zum Thema: Variablen

---
+++ variablen/arithmetic.py
@@ -0,0 +1,41 @@
+"""
+
+    Arithmetik
+
+"""
+zahl1 = 10
+zahl2 = 5
+
+# integer addieren
+ergebnis = zahl1 + zahl2
+print(ergebnis)
+
+# subtrahieren
+ergebnis = zahl1 - zahl2
+print(ergebnis)
+
+# multiplizieren
+print(zahl1 * zahl2)
+
+# exponent
+print(zahl1 ** zahl2)
+
+# division
+div = zahl1 / zahl2
+print(div)
+print(type(div))
+
+# modulo
+mod = 10 % 3
+print("modulo", mod)
+
+# division und komma abschneiden
+div2 = 10 // 3
+print(div2)
+print(type(div2))
+
+# addition von bool
+bool1 = True
+bool2 = True
+print(bool1 + bool2)
+
---
+++ variablen/precision.py
@@ -0,0 +1,31 @@
+
+
+wert = 0
+
+wert += 0.12
+wert += 2.1
+wert += 3.1
+wert += 3.1
+wert += 3.1
+wert += 0.1
+wert += 0.1
+wert += 0.1
+wert += 0.1
+wert += 0.1
+wert += 0.1
+wert += 0.1
+wert += 0.1
+wert += 0.1
+wert += 0.1
+wert += 0.1
+
+print(wert)
+print(round(wert, 2))
+
+################################
+print(49.0 * (1.0 / 49.0) )  # bei multiplikation und diviosn auch vorsichtig sein
+
+print((1/49)*49) ## ja ... das ist natürlich nicht gut...
+
+
+
